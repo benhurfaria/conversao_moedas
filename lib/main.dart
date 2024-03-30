b3646da1b35c741c1ac003c3137c8f8d8a779f9b
benhurfaria <benhurfaria3@gmail.com>
2024-03-30 19:29:24 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children:[
+              Container(color: Colors.red,),
+              Container(color: Colors.blue, height: 100,),
+            ],
+          ),
+    );
+  }
+}
